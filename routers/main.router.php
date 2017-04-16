04eb1d874afab12a40fdc2190c52bd2ad77a0411
“PosTrick” <postrick.info@gmail.com>
2017-04-16 23:40:25 +0200

    Requests: post,put,delete

--- routers/main.router.php
+++ routers/main.router.php
@@ -3,10 +3,26 @@
 require __DIR__ . '/../lib/HttpUtils.php';
 include __DIR__ . '/../models/User.php';
 
-$app->get('/[{name}]', function ($request, $response, $args) {
+$app->get('/methodget', function ($request, $response, $args) {
 
     $userClass = new User();
     $response = $userClass->getUsers();
 
     \models\HttpUtils::echoResponse(200,$response);
+});
+
+$app->post('/methodpost', function($request, $response) {
+    $data = $request->getParsedBody();
+    print_r($data['email']);
+});
+
+$app->put('/methodput/{id}', function($request, $response) {
+    $id = $request->getAttribute('id');
+    $data = $request->getParsedBody();
+    print_r("Put: ". $data['email'] . ' - ' . $id);
+});
+
+$app->delete('/methoddelete/{id}', function($request, $response) {
+    $id = $request->getAttribute('id');
+    print_r("Delete: " . $id);
 });
